2a3bb1670f83e391de9dfe3c4e6d87e227a860cc
krsnamara <martinj.fitzpatrick@gmail.com>
2023-11-11 12:56:26 -0500

    building out the job posting information

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,8 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { RAPID_API_KEY } from '@env';
 
-const rapidApiKey = RAPID_API_KEY;
+const rapidApiKey = process.env.EXPO_PUBLIC_RAPID_API_KEY;
 
 const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
@@ -13,7 +12,7 @@ const useFetch = (endpoint, query) => {
     method: 'GET',
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      'X-RapidAPI-Key': RAPID_API_KEY,
+      'X-RapidAPI-Key': rapidApiKey,
       'X-RapidAPI-Host': 'jsearch.p.rapidapi.com',
     },
     params: { ...query },
@@ -45,3 +44,5 @@ const useFetch = (endpoint, query) => {
 
   return { data, isLoading, error, refetch };
 };
+
+export default useFetch;
